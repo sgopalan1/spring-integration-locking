bede53e5495d8bf80c2c210a1bc49babfed41fc0
Srinivasan Gopalan <srinivasan.gopalan@nytimes.com>
2021-07-08 07:19:28 -0400

    Adding Spring Batch jobs, Leader selection and making one job require a leader to run

--- src/main/java/com/example/distributedlockswithspringintegration/Scheduler.java
+++ src/main/java/com/example/distributedlockswithspringintegration/Scheduler.java
@@ -1,2 +1,29 @@
-package com.example.distributedlockswithspringintegration;public class Scheduler {
+package com.example.distributedlockswithspringintegration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.support.leader.LockRegistryLeaderInitiator;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scheduler {
+    @Autowired
+    LockRegistryLeaderInitiator leaderInitiator;
+
+    @Scheduled(initialDelay = 2000, fixedDelay = 1500)
+    public void scheduleTask1(){
+        System.out.println("Executing task 1");
+    }
+
+    @Scheduled(initialDelay = 1000, fixedDelay = 3000)
+    public void scheduleTask2(){
+        // This will only run if this instance is "the leader"
+        if (!isLeader()) return;
+
+        System.out.println("Executing task 2");
+    }
+
+    private boolean isLeader() {
+        return leaderInitiator.getContext().isLeader();
+    }
 }
